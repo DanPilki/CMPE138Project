9838845ea36433a7b0b01032ddef4fd2b061728d
previnw <previn.wong@sjsu.edu>
2016-11-26 21:18:30 -0800

    new home page

--- Warriors/config/routes.rb
+++ Warriors/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+  get 'welcome/home'
 
-  root 'welcome#index'
+  root 'welcome#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
