b655031cdae53c83b95986c06155cda4b310a973
powerbygood <powerbygood@yahoo.com>
2016-11-28 23:31:13 -0800

    test files

--- Warriors1.2/config/routes.rb
+++ Warriors1.2/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'welcome/index'
+  #get 'welcome/index'
+  match ':controller(/:action(/:id))', :via => :get
 
   root 'welcome#index'
 end
