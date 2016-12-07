75aa482d438fba5ed09e19c1669e926211d44b63
powerbygood <powerbygood@yahoo.com>
2016-12-07 03:55:09 +0000

    update

--- Warriors1.2/app/models/stat.rb
+++ Warriors1.2/app/models/stat.rb
@@ -1,9 +1,3 @@
 class Stat < ApplicationRecord
-  validates: Point, presence: true
-  validates: Assist, presence: true
-  validates: Rebound, presence: true
-  validates: Steal, presence: true
-  validates: Foul, presence: true
-  validates: Players, presence: true
-  validates: Games, presence: true
+  validates :point, presence: true
 end
